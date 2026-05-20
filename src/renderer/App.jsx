997b30ca6265035e54e068e2ae6c2a583e115c13
Eduardo Lillo <eduardolillom@gmail.com>
2026-05-20 14:36:59 -0400

    Layout component

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Header from './components/Header';
+import Header from './components/layout/Header';
 import MatrizTab from './components/MatrizTab';
 import JugadoresTab from './components/JugadoresTab';
 import GuildsTab from './components/GuildsTab';
